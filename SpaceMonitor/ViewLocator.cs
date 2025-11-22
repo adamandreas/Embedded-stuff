ec2c7b017cb20c4e98004df0c9f3e6b8c87a2524
Adam Andreas <adam.a.tonsberg@gmail.com>
2025-11-22 16:48:04 +0300

    refactor: Minor code style and documentation updates

--- SpaceMonitor/ViewLocator.cs
+++ SpaceMonitor/ViewLocator.cs
@@ -7,11 +7,12 @@ namespace SpaceMonitor;
 
 public class ViewLocator : IDataTemplate
 {
+
     public Control? Build(object? param)
     {
         if (param is null)
             return null;
-
+        
         var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
         var type = Type.GetType(name);
 
@@ -19,7 +20,7 @@ public class ViewLocator : IDataTemplate
         {
             return (Control)Activator.CreateInstance(type)!;
         }
-
+        
         return new TextBlock { Text = "Not Found: " + name };
     }
 
